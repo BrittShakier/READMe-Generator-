8e9a6d989f631eb3591857f842cc90eec050f4ef
britjames7@gmail.com <britjames7@gmai.com>
2024-05-14 18:35:51 -0400

    new code

--- develop/index.js
+++ develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-
-const fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require("fs");
+const generateMarkdown = require ("utils/generateMarkdown.js")
 
 
 // TODO: Create an array of questions for user input
@@ -55,10 +55,25 @@ const questions = [{
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) { 
+    return fs.writeFile('README.md', questions(data), (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md file has been created!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init(questions) {
+    inquirer.prompt([
+        
+    ]).then((answers) => {
+       
+        const readmeContent = questions(answers);
+ })
+}
 
 // Function call to initialize app
 init();
